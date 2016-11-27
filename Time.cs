1a624f6c34150e71aef02300a089f99a193d1082
SamuelAlonso <senreengine@gmail.com>
2016-11-27 17:24:31 +0100

    Update Time.cs

--- Time.cs
+++ Time.cs
@@ -4,6 +4,13 @@ namespace SenreEngine
 {
     public class Time
     {
-        
+        public static float TimeSinceCallbackThisFunction()
+        {
+            Stopwatch SW = new Stopwatch();
+            while (true)
+            {
+                return SW.ElapsedMilliseconds;
+            }
+        }
     }
 }
