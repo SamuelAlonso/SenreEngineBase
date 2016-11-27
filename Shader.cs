51ba82242edd03cc403d7a300d905aeadc83ff25
SamuelAlonso <senreengine@gmail.com>
2016-11-27 17:18:38 +0100

    Update Shader.cs

--- Shader.cs
+++ Shader.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.IO;
 using System;
+using System.Collections.Generic;
 
 namespace SenreEngine
 {
@@ -12,6 +13,9 @@ namespace SenreEngine
         private int fragmentID = 0;
         private int geometryID = 0;
         private int computeID = 0;
+
+        public Tuple<List<string>, int> ShaderAddUniform;
+        public List<Type> VarsToAddInUniform;
 
         private int LoadShader(string file_without_path, int type)
         {
@@ -53,6 +57,67 @@ namespace SenreEngine
             GL.CompileShader(ShaderId);
             return ShaderId;
         }
+        private void BindUniform()
+        {
+            for (int program = 0; program < ShaderAddUniform.Item2; program ++)
+            {
+                Type VarToAdd = null;
+                int VarToAddCount = VarsToAddInUniform.Count;
+                for(int x = 0; x < VarToAddCount; x++)
+                {
+                    VarToAdd = VarsToAddInUniform[x];
+                }
+                foreach (string varname in ShaderAddUniform.Item1)
+                {
+
+                    int location = GL.GetUniformLocation(programID, varname);
+
+                    
+                        if (VarToAdd == typeof(int))
+                        {
+                            GL.Uniform1(location, Convert.ToInt32(VarToAdd));
+                        }
+                        else if (VarToAdd == typeof(double))
+                        {
+                            GL.Uniform1(location, Convert.ToDouble(VarToAdd));
+                        }
+                        else if (VarToAdd == typeof(float))
+                        {
+                            GL.Uniform1(location, Convert.ToSingle(VarToAdd));
+                        }
+                        else if (VarToAdd == typeof(Vector2))
+                        {
+                            Vector2 Var = (Vector2)Convert.ChangeType(new Vector2(), VarToAdd);
+
+                            GL.Uniform2(location, 1, new float[2] { Var.x, Var.y });
+
+                        }
+                        else if (VarToAdd == typeof(Vector3))
+                        {
+                            Vector3 Var = (Vector3)Convert.ChangeType(new Vector3(), VarToAdd);
+
+                            GL.Uniform3(location, 1, new float[3] { Var.x, Var.y, Var.z });
+                        }
+                        else if (VarToAdd == typeof(Vector4))
+                        {
+                            Vector4 Var = (Vector4)Convert.ChangeType(new Vector4(), VarToAdd);
+
+                            GL.Uniform4(location, 1, new float[4] { Var.x, Var.y, Var.z, Var.w });
+                        }
+                        else if (VarToAdd == typeof(Matrix3x3))
+                        {
+                            Matrix3x3 m = (Matrix3x3)Convert.ChangeType(new Matrix3x3(), VarToAdd);
+                            GL.UniformMatrix3(location, 1, false, m.ToDoubleArray(m));
+                        }
+                        else if (VarToAdd == typeof(Matrix4x4))
+                        {
+                            Matrix4x4 m = (Matrix4x4)Convert.ChangeType(new Matrix4x4(), VarToAdd);
+                            GL.UniformMatrix4(location, 1, false, m.ToDoubleArray(m));
+                        
+                    }
+                }
+            }
+        }
         public Shader(string vertexfile, string fragmentfile, string computefile = null, string geometryfile = null)
         {
             vertexID = LoadShader(vertexfile, 0);
@@ -65,7 +130,14 @@ namespace SenreEngine
             {
                 computeID = LoadShader(computefile, 3);
             }
+            
             programID = GL.CreateProgram();
+
+            if(programID == ShaderAddUniform.Item2)
+            {
+                BindUniform();
+            }
+
             GL.AttachShader(programID, vertexID);
             GL.AttachShader(programID, fragmentID);
             if(geometryfile != null)
